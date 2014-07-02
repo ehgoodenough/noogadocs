f55a61cb72634d88c0042fad590ca061ccf1f413
Andrew McPherson <andrewmcp333@gmail.com>
2014-07-02 12:45:47 -0400

    Reworked some of the structure for the techtalk.

--- NoogaDocs.js
+++ NoogaDocs.js
@@ -1,21 +1,41 @@
-var docs = new Meteor.Collection("docs");
+docs = new Meteor.Collection("docs")
 
 if(Meteor.isClient)
 {
 	Session.set("selected", undefined);
 	
-	Template.content.selected = function()
+	Template.noogadocs.selected = function()
 	{
 		return Session.get("selected");
 	}
 	
-	Template.content.events(
-	{
+	Template.noogadocs.events({
 		"click button": function()
 		{
 			var doc = {name: "untitled doc", text: ""};
 			var _id = docs.insert(doc);
 			Session.set("selected", _id);
+		}
+	});
+	
+	Template.browse.docs = function()
+	{
+		return docs.find({}, {sort: {name: 1}});
+	}
+	
+	Template.browse.selected = function()
+	{
+		if(this._id == Session.get("selected"))
+		{
+			return "selected";
+		}
+	}
+	
+	Template.browse.events(
+	{
+		"click li": function()
+		{
+			Session.set("selected", this._id);
 		}
 	});
 	
@@ -30,36 +50,13 @@ if(Meteor.isClient)
 		"keyup textarea": function(event)
 		{
 			var value = event.target.value;
-			var _id = Session.get("selected");
-			docs.update(_id, {$set: {text: value}});
+			docs.update(this._id, {$set: {text: value}});
 		},
-		"click span": function(event)
+		"click span": function()
 		{
-			var _id = Session.get("selected")
-			var name = docs.findOne(_id).name;
+			var name = docs.findOne(this._id).name;
 			var value = prompt("Rename the doc?", name) || name;
-			docs.update(_id, {$set: {name: value}});
-		}
-	});
-	
-	Template.view.list = function()
-	{
-		return docs.find({}, {sort: {name: 1}});
-	}
-	
-	Template.view.selected = function()
-	{
-		if(this._id == Session.get("selected"))
-		{
-			return "selected";
-		}
-	}
-	
-	Template.view.events(
-	{
-		"click li": function()
-		{
-			Session.set("selected", this._id);
+			docs.update(this._id, {$set: {name: value}});
 		}
 	});
 }
